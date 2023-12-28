d1c4aad88de1c819123759cb49232c92c85b4500
sakulstar <111194775+sakulstar@users.noreply.github.com>
2023-12-28 13:44:09 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-center font-semibold text-2xl">Welcome to the first commit of <a target="_blank" href="https://github.com/muddled-live/Reading-List" class="text-orange-500">Reading-List</a>!</h1>
+<p class="text-center font-semibold text-2xl">Visit <a href="https://kit.svelte.dev" class="text-orange-500">kit.svelte.dev</a> to read the documentation</p>
