d9efd72b35a1a7fd44bb5b36cf55704b125b136b
sakulstar <111194775+sakulstar@users.noreply.github.com>
2023-12-28 14:34:06 +0100

    Changed prettier config and formatted files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="text-center font-semibold text-2xl">Welcome to the first commit of <a target="_blank" href="https://github.com/muddled-live/Reading-List" class="text-orange-500">Reading-List</a>!</h1>
-<p class="text-center font-semibold text-2xl">Visit <a href="https://kit.svelte.dev" class="text-orange-500">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-center font-semibold text-2xl">
+	Welcome to the first commit of <a
+		target="_blank"
+		href="https://github.com/muddled-live/Reading-List"
+		class="text-orange-500">Reading-List</a
+	>!
+</p>
+<p class="text-center font-semibold text-2xl">
+	Visit <a target="_blank" href="https://kit.svelte.dev" class="text-orange-500">kit.svelte.dev</a
+	> to read the documentation
+</p>
